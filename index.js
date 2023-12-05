d577762ac26a4e4756be61352df3828b281f6f70
Daisy Sanchez <ddaaiissyy1013@gmail.com>
2023-12-05 00:05:54 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards (names, event ) {
+const thankYouForTheBirthdayGift= [];
+for (let i = 0; i < names.length; i++) {
+thankYouForTheBirthdayGift.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+ }
+    return thankYouForTheBirthdayGift;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
